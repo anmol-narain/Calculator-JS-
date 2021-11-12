936df8f732e49fcd79b691c42e6d47164f5c3cde
anmol-narain <anmolnarain83@gmail.com>
2021-11-12 11:34:33 +0530

    changes js

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 let displayScreen = document.getElementById('displayScreen');
 buttons = document.querySelectorAll('button');
 let answer = "";
+let finalAns = "";
+let buttonArray = [];
 for(item of buttons){
     item.addEventListener('click', (e)=>{
         // grab the text inside the button first
         buttonText = e.target.innerText;
+        // if(buttonText !== 'DEL'){
+        //     buttonArray.push(buttonText);
+        // }
         console.log("You just clicked ", buttonText);
 
         if(buttonText == 'X'){
             buttonText = '*';
             answer += buttonText;
             displayScreen.value = answer; 
+            
         }
         else if (buttonText == 'C'){
             answer = "";
             displayScreen.value = answer;
         }
         else if (buttonText == '='){
-            displayScreen.value = eval(answer);
+            finalAns = eval(answer);
+            displayScreen.value = finalAns;
+        }
+        else if(buttonText == 'DEL'){
+            answer = answer.slice(0, -1);
+            displayScreen.value = answer;
         }
         else{
             answer += buttonText; 
